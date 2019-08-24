50ab9dc64a73a037b7557a055c279446ece0dd7f
mohammed zourob <fxfddfno@gmail.com>
2019-08-24 18:54:25 +0300

    stup our server

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
-const port = env;
+mongoose
+  .connect("mongodb://localhost:27018/shop-restful-api", {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(_ => {
+    console.log("Connect to mongoose port: 27018");
+  })
+  .then(_ => {
+    const port = 3000 || process.env.PORT;
+    app.listen(port, () => {
+      console.log(`server run port: ${port}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+/**
+ * user
+ * order
+ * product
+ *
+ */
