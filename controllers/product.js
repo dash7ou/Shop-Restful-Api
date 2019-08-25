89ac16da53e2177bce5698e5dd8549cebf6fffd2
mohammed zourob <fxfddfno@gmail.com>
2019-08-25 15:01:12 +0300

    add update and delete product

--- controllers/product.js
+++ controllers/product.js
@@ -82,9 +82,12 @@ exports.postProduct = async (req, res, next) => {
       price: price,
       author: user._id
     });
-
     await product.save();
+    const productNew = [{ product: product._id, quantity: 1 }];
 
+    user.products.items.push(...productNew);
+
+    await user.save();
     res.send({
       message: "Successfully create new product",
       product: product
@@ -97,6 +100,87 @@ exports.postProduct = async (req, res, next) => {
   }
 };
 
-exports.putProduct = async (req, res, next) => {};
+exports.putProduct = async (req, res, next) => {
+  try {
+    const allowUpdate = ["name", "photo", "description", "price"];
+    const requireUpdate = Object.keys(req.body);
 
-exports.deleteProduct = async (req, res, next) => {};
+    const isValidateToUpdate = requireUpdate.every(update =>
+      allowUpdate.includes(update)
+    );
+
+    if (!isValidateToUpdate) {
+      const error = {};
+      error.message = new Error("make sure what you want to update :)");
+      error.statusCode = 422;
+      error.data = "";
+      throw error;
+    }
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+    if (!product) {
+      const error = {};
+      error.message = new Error("No product to delete it..");
+      error.statusCode = 422;
+      error.data = "";
+      throw error;
+    }
+    if (product.author.toString() !== req.userId.toString()) {
+      const error = {};
+      error.message = new Error(
+        "This user cant update this product you are not owner.."
+      );
+      error.statusCode = 422;
+      error.data = "";
+      throw error;
+    }
+
+    requireUpdate.forEach(update => (product[update] = req.body[update]));
+    await product.save();
+    res.send({
+      message: "successfully update product!",
+      product: product
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
+
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      const error = {};
+      error.message = new Error("No product to delete it..");
+      error.statusCode = 404;
+      error.data = "";
+      throw error;
+    }
+    if (product.author.toString() !== req.userId.toString()) {
+      const error = {};
+      error.message = new Error(
+        "This user cant delete this product you are not owner.."
+      );
+      error.statusCode = 422;
+      error.data = "";
+      throw error;
+    }
+
+    await Product.findOneAndDelete({
+      _id: product._id,
+      author: req.userId
+    });
+    res.send({
+      message: "Deleted Product!",
+      product: product
+    });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
--- routers/product.js
+++ routers/product.js
@@ -6,7 +6,7 @@ const isAuth = require("../middleware/auth");
 router.get("/", isAuth, productController.getProducts);
 router.get("/:id", isAuth, productController.getProduct);
 router.post("/", isAuth, productController.postProduct);
-router.put("/product/:id", isAuth, productController.putProduct);
-router.delete("/product/:id", isAuth, productController.deleteProduct);
+router.put("/:id", isAuth, productController.putProduct);
+router.delete("/:id", isAuth, productController.deleteProduct);
 
 module.exports = router;
